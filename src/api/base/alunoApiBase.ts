04e6df91dc8b9e6e254c98926dc470bed34c6d23
Guilherme Sandoval <guijpjs@gmail.com>
2025-04-11 21:07:07 -0300

    Fix call API

--- src/api/base/alunoApiBase.ts
+++ src/api/base/alunoApiBase.ts
@@ -1,11 +1,5 @@
 import { AlunoResponse } from "../models/response/alunoResponse";
 
 export abstract class IAlunoAPI {
-  protected readonly baseRoute: string;
-
-  constructor(baseRoute: string) {
-    this.baseRoute = baseRoute;
-  }
-
   abstract getAlunos(): Promise<AlunoResponse[]>;
 }
