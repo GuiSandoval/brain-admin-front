eb7057c78719ce3804b6ae7dd41163003daf5efd
Guilherme Sandoval <guijpjs@gmail.com>
2025-06-26 18:52:14 -0300

    Structure <CalendarPage />

--- src/app/(private)/calendario/page.tsx
+++ src/app/(private)/calendario/page.tsx
@@ -1,8 +1,14 @@
+import LayoutColumns from "@/components/layoutColumns/layoutColumns";
 import * as S from "./styles";
+import PageTitle from "@/components/pageTitle/pageTitle";
 export default function Calendario() {
   return (
     <S.Container>
-      <h1>Pagina Calendario</h1>
+      <PageTitle title="Calendar" description="Manage your schedule and upcoming events" />
+      <LayoutColumns sizeLeft="70%" sizeRight="30%">
+        <div>Calendario</div>
+        <div>Sidebar</div>
+      </LayoutColumns>
     </S.Container>
   );
 }
